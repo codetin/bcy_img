931329981184151e0aed0d183540f3cf97aaa74b
code_tin <code_tin@msn.com>
2018-03-06 16:44:31 +0800

    change pic_path

--- scrapy_test/pipelines.py
+++ scrapy_test/pipelines.py
@@ -18,7 +18,7 @@ class ScrapyTestPipeline(object):
 class ImagesPipeline(ImagesPipeline):
     def get_media_requests(self, item, info):
         for image_url in item['image_urls']:
-            yield scrapy.Request(image_url,meta={'item':item,'index':item['image_urls'].index(image_url)})
+            yield scrapy.Request(image_url,meta={'uid':item['uid'],'album_id':item['album_id']})
 
 
     #完成item下载之后进行改名
@@ -37,25 +37,28 @@ class ImagesPipeline(ImagesPipeline):
         if not image_paths:
             raise DropItem("Item contains no images")
         for path in image_paths:
-            move_path=dict()
-            move_path['old']=os.getcwd() + '/pic/' + path
-            move_path['new']=os.getcwd() + '/pic' + item['file_path'] + path
-            #./pic/2104760/2079169/full/1df239329d83a6d572bec40140b3faa12dbe269a.jpg
-            print (move_path)
-            os.renames(move_path['old'],move_path['new'])
-            sql = "INSERT INTO bcy_img (auth_id,album_id,pic_path) VALUES (%s,%s,%s)"
-            cursor.execute(sql,(item['uid'],item['album_id'],'/pic' + item['file_path'] + path))
+
             #print (conn.insert_id())
+
+            # move_path=dict()
+            # move_path['old']=os.getcwd() + '/pic/' + path
+            # move_path['new']=os.getcwd() + '/pic' + item['file_path'] + path
+            #./pic/2104760/2079169/full/1df239329d83a6d572bec40140b3faa12dbe269a.jpg
+
+            # print (move_path)
+            # os.renames(move_path['old'],move_path['new'])
+
+            sql = "INSERT INTO bcy_img (auth_id,album_id,pic_path) VALUES (%s,%s,%s)"
+            cursor.execute(sql,(item['uid'],item['album_id'],'/pic/' + path))
             cursor.connection.commit()
-        return item
+
+            return item
     
 
-#    def file_path(self,request,response=None,info=None):
-#        item=request.meta['item'] #通过上面的meta传递过来item
-#        index=request.meta['index'] #通过上面的index传递过来列表中当前下载图片的下标
-#
-#        #图片文件名，item['carname'][index]得到汽车名称，request.url.split('/')[-1].split('.')[-1]得到图片后缀jpg,png
-#        image_guid = item['image_paths'][index]+'.jpg'
-#        #图片下载目录 此处item['country']即需要前面item['country']=''.join()......,否则目录名会变成\u97e9\u56fd\u6c7d\u8f66\u6807\u5fd7\xxx.jpg
-#        filename = u'full/{0}/{1}'.format('name', image_guid) 
-#        return filename            
+    def file_path(self,request,response=None,info=None):
+        uid = request.meta['uid'] #通过上面的meta传递过来item
+        album_id = request.meta['album_id'] #通过上面的index传递过来列表中当前下载图片的下标
+        #图片文件名，item['carname'][index]得到汽车名称，request.url.split('/')[-1].split('.')[-1]得到图片后缀jpg,png
+        image_guid = uid+'/'+album_id+'/'+request.url.split('/')[-1]
+        #图片下载目录 此处item['country']即需要前面item['country']=''.join()......,否则目录名会变成\u97e9\u56fd\u6c7d\u8f66\u6807\u5fd7\xxx.jpg
+        return 'full/%s' % (image_guid)
